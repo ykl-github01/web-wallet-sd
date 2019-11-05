b4d5ddaa75d38f05e506784d8d8e83b9dedc6496
ykl-github01 <171765598@qq.com>
2019-11-05 19:13:07 +0800

    wallet-auth

--- process/no_enough_amount_process.py
+++ process/no_enough_amount_process.py
@@ -4,6 +4,9 @@ from case.creat_wallet import CreatWallet
 from case.get_coin import GetCoin
 from case.get_hidden_coin import GetHiddenCoin
 from case.no_enough_public_address_hidden_amount import NoEnoughPublicAddressHiddenAmount
+from case.no_enough_private_address_hidden_amount import NoEnoughPrivateAddressHiddenAmount
+from case.no_enough_public_address_amount import NoEnoughPublicAddressAmount
+from case.no_enough_private_address_amount import NoEnoughPrivateAddressAmount
 from case.check_view_account_info import CheckViewAccountInfo
 import time,os,HTMLTestReportCN
 class WebWalletProcess(unittest.TestCase):
@@ -20,8 +23,17 @@ class WebWalletProcess(unittest.TestCase):
     def test_2check_view_account_info(self):
         CheckViewAccountInfo().check_view_account_info()
 
-    def test_2no_enough_public_address_hidden_amount(self):
+    def test_3no_enough_public_address_amount(self):
+        NoEnoughPublicAddressAmount().no_enough_public_address_amount()
+
+    def test_4no_enough_public_address_hidden_amount(self):
         NoEnoughPublicAddressHiddenAmount().no_enough_public_address_hidden_amount()
+
+    def test_5no_enough_private_address_amount(self):
+        NoEnoughPrivateAddressAmount().no_enough_private_address_amount()
+
+    def test_6no_enough_private_address_hidden_amount(self):
+        NoEnoughPrivateAddressHiddenAmount().no_enough_private_address_hidden_amount()
 
     def tearDown(self):
         pass
@@ -36,7 +48,12 @@ if __name__ == '__main__':
     testsuite=unittest.TestSuite()
     testsuite.addTest(WebWalletProcess('test_0creat_wallet'))
     testsuite.addTest(WebWalletProcess('test_1getcoin'))
-    testsuite.addTest(WebWalletProcess('test_2no_enough_public_address_hidden_amount'))
+    testsuite.addTest(WebWalletProcess('test_2check_view_account_info'))
+    testsuite.addTest(WebWalletProcess('test_3no_enough_public_address_amount'))
+    testsuite.addTest(WebWalletProcess('test_4no_enough_public_address_hidden_amount'))
+    testsuite.addTest(WebWalletProcess('test_5no_enough_private_address_amount'))
+    testsuite.addTest(WebWalletProcess('test_6no_enough_private_address_hidden_amount'))
+
     runner=HTMLTestReportCN.HTMLTestRunner(
 
         stream=fp,
